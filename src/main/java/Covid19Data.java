4a72510b9d85e2f274ef9845d62e23dadbebd0df
Mathias-Thomsen <math831t6@stud.kea.dk>
2022-11-02 12:52:08 +0100

    First commit

---
+++ src/main/java/Covid19Data.java
@@ -0,0 +1,88 @@
+import java.time.LocalDate;
+import java.util.Date;
+
+
+public class Covid19Data {
+    private String region;
+    private String aldersGruppe;
+    private int bekræftedeTilfældeIAlt;
+    private int døde;
+    private int indlagtePåIntensivAfdeling;
+    private int indlagte;
+    private String dato; //TODO: lav måske om til localdate
+
+    public Covid19Data(){
+
+    }
+    public Covid19Data(String region, String aldersGruppe, int bekræftedeTilfældeIAlt, int døde, int indlagtePåIntensivAfdeling, int indlagte, String dato) {
+        this.region = region;
+        this.aldersGruppe = aldersGruppe;
+        this.bekræftedeTilfældeIAlt = bekræftedeTilfældeIAlt;
+        this.døde = døde;
+        this.indlagtePåIntensivAfdeling = indlagtePåIntensivAfdeling;
+        this.indlagte = indlagte;
+        this.dato = dato;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getAldersGruppe() {
+        return aldersGruppe;
+    }
+
+    public void setAldersGruppe(String aldersGruppe) {
+        this.aldersGruppe = aldersGruppe;
+    }
+
+    public int getBekræftedeTilfældeIAlt() {
+        return bekræftedeTilfældeIAlt;
+    }
+
+    public void setBekræftedeTilfældeIAlt(int bekræftedeTilfældeIAlt) {
+        this.bekræftedeTilfældeIAlt = bekræftedeTilfældeIAlt;
+    }
+
+    public int getDøde() {
+        return døde;
+    }
+
+    public void setDøde(int døde) {
+        this.døde = døde;
+    }
+
+    public int getIndlagtePåIntensivAfdeling() {
+        return indlagtePåIntensivAfdeling;
+    }
+
+    public void setIndlagtePåIntensivAfdeling(int indlagtePåIntensivAfdeling) {
+        this.indlagtePåIntensivAfdeling = indlagtePåIntensivAfdeling;
+    }
+
+    public int getIndlagte() {
+        return indlagte;
+    }
+
+    public void setIndlagte(int indlagte) {
+        this.indlagte = indlagte;
+    }
+
+    public String getDato() {
+        return dato;
+    }
+
+    public void setDato(String dato) {
+        this.dato = dato;
+    }
+
+
+
+
+
+    }
+
---
+++ src/main/java/Filehandler.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Filehandler {
+
+    public ArrayList<Covid19Data> loadData() throws IOException {
+        ArrayList<Covid19Data> covid19DataArrayList = new ArrayList<>();
+        Scanner reader = new Scanner(new File("data\\11_noegletal_pr_region_pr_aldersgruppe.csv"), StandardCharsets.ISO_8859_1);
+
+        while(reader.hasNextLine()){
+            String line = reader.nextLine();
+            covid19DataArrayList.add(parseCsvLine(line));
+
+        }
+        return covid19DataArrayList;
+
+    }
+
+    private Covid19Data parseCsvLine(String line) {
+        String[] parts = line.split(";");
+
+        Covid19Data dataObjekt = new Covid19Data();
+        dataObjekt.setRegion(parts[0]);
+
+        dataObjekt.setAldersGruppe(parts[1]);
+        dataObjekt.setBekræftedeTilfældeIAlt(Integer.parseInt(parts[2]));
+        dataObjekt.setDøde(Integer.parseInt(parts[3]));
+        dataObjekt.setIndlagtePåIntensivAfdeling(Integer.parseInt(parts[4]));
+        dataObjekt.setIndlagte(Integer.parseInt(parts[5]));
+        dataObjekt.setDato(parts[6]);
+
+        return dataObjekt;
+    }
+
+}
